20258bb4590af31d27d3ac8e8dad2517328df845
Sukhdev Singh <41sukhdevsingh@gmail.com>
2022-10-18 18:29:00 +0530

    Add build step to build react

---
+++ scripts/build.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+require("esbuild").build({
+    logLevel: "debug",
+    entryPoints: ['src/index.js'],
+    bundle: true,
+    target: ['es2020',
+        'chrome58',
+        'edge16',
+        'firefox57',
+        'node12',
+        'safari11',],
+    loader: {'.js': 'jsx'},
+    outfile: "dist/out.js"
+}).catch((e) => {
+    console.error(e)
+    process.exit(1)
+})
