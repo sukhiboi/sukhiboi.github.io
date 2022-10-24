b7db56ed663940c4845787f07445dcb01a06c49c
Sukhdev Singh <41sukhdevsingh@gmail.com>
2022-10-24 11:28:09 +0530

    Add theme with styled components

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import lightTheme from "./theme/theme.light";
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background: ${lightTheme.background};
+  }
+`;
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={lightTheme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
