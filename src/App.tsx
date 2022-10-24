169c4220f76a45780515556d3c83d5ffc1adf87c
Sukhdev Singh <41sukhdevsingh@gmail.com>
2022-10-24 11:12:53 +0530

    Add theme values

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,12 @@
-export default () => <h1>Running with Vite</h1>;
+import BlogPost from "./components/BlogPost/BlogPost";
+
+export default () => (
+  <div>
+    <h1>Blog</h1>
+    <main>
+      {Array.from({ length: 10 }).map(() => (
+        <BlogPost />
+      ))}
+    </main>
+  </div>
+);
---
+++ src/components/BlogPost/BlogPost.tsx
@@ -0,0 +1,3 @@
+export default () => {
+  return <h3>Blog Post</h3>;
+};
